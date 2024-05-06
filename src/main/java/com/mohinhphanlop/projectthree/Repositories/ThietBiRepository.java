c42e0a6f4194bb06bf3193f82537a41204e2bbb5
Tiền Minh Vy <tienminhvydev@gmail.com>
2024-05-06 10:52:21 +0700

    Sửa lỗi thanh tìm kiếm

--- src/main/java/com/mohinhphanlop/projectthree/Repositories/ThietBiRepository.java
+++ src/main/java/com/mohinhphanlop/projectthree/Repositories/ThietBiRepository.java
@@ -4,8 +4,11 @@ import com.mohinhphanlop.projectthree.Models.ThietBi;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Repository;
 
@@ -15,9 +18,12 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface ThietBiRepository
-                extends PagingAndSortingRepository<ThietBi, Integer>, CrudRepository<ThietBi, Integer> {
+                extends JpaRepository<ThietBi, Integer>,
+                PagingAndSortingRepository<ThietBi, Integer>, CrudRepository<ThietBi, Integer> {
 
         Page<ThietBi> findAll(@PageableDefault(value = 10, page = 0) Pageable pageable);
 
-        Page<ThietBi> findAllByTenTB(String tenTB, @PageableDefault(value = 10, page = 0) Pageable pageable);
+        @Query("SELECT t FROM ThietBi t WHERE t.tenTB LIKE %:tenTB%")
+        Page<ThietBi> findAllByTenTB(@Param("tenTB") String tenTB,
+                        @PageableDefault(value = 10, page = 0) Pageable pageable);
 }
